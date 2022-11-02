e841809b779e11d1c068cca85b5287cb61458d94
EarthSquirrel <britneyandcaddy1@gmail.com>
2022-11-02 17:14:47 -0600

    REact demo

---
+++ reactdemo/src/Counter.jsx
@@ -0,0 +1,13 @@
+// must return only one element (threefore put everything in a div of all
+// elements you want to return
+const Counter = () => {
+    return (
+        <div>
+            Counter willl go here
+
+        </div>
+    )
+}
+
+export default Counter;
+
